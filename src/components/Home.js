39827764050f0d3f898032567486a0a79fd00d22
= <=>
2019-03-02 04:36:51 -0700

    Get create-react-app, react-router, redux development environment ready.

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+
+      </div>
+    );
+  }
+}
+
+export default Home;
